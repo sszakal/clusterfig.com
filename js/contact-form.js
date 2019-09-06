ec4f4fb27b2d700ff984ebe4c38e7341597bc95a
stefan.sz <stefan.szakal@clusterfig.com>
2019-09-06 15:58:17 +0100

    mailgun directly in page

--- js/contact-form.js
+++ js/contact-form.js
@@ -30,13 +30,13 @@ $(document).ready(function(){
         if (proceed) {
             //data to be sent to server
             post_data = {
-                'from': atob('Y2x1c3RlcmZpZy5jb20gPHNhbmRib3g4N2JlOWZmMWNhYjA0MDNlOTA5YzhlODljZmY3NGNjNC5tYWlsZ3VuLm9yZw=='),
+                'from': atob('Y2x1c3RlcmZpZy5jb20gPG1haWxndW5AYXBpLm1haWxndW4ubmV0L3YzL3NhbmRib3g1NjQxYWE1OGRlNmE0YTVjOWVhNmE5MmY0MmMwMTIwZi5tYWlsZ3VuLm9yZz4='),
                 'to': atob('c3RlZmFuLnN6YWthbEBjbHVzdGVyZmlnLmNvbQ=='),
                 'subject': "clusterfig.com contact form",
                 'text': "Name:" + user_name + "\n Email: " + user_email + "\nMessage:" + user_message 
             };
 
-            var url = atob('aHR0cHM6Ly9hcGk6NDE2N2MzODItZGFhZGQyMTJAYXBpLm1haWxndW4ubmV0L3YzL3NhbmRib3g4N2JlOWZmMWNhYjA0MDNlOTA5YzhlODljZmY3NGNjNC5tYWlsZ3VuLm9yZy9tZXNzYWdlcw==')
+            var url = atob('aHR0cHM6Ly9hcGk6NDE2N2MzODItZGFhZGQyMTJAYXBpLm1haWxndW4ubmV0L3YzL3NhbmRib3g1NjQxYWE1OGRlNmE0YTVjOWVhNmE5MmY0MmMwMTIwZi5tYWlsZ3VuLm9yZy9tZXNzYWdlcw==')
 
             //Ajax post data to server
             $.post(url, post_data, function(response){
